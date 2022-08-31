08e99d5139d8dda7473cbe1201459869eb2c8f46
Pol Imbing <pol.imbing@xurpas.com>
2022-08-31 12:04:38 +0800

    deployment test

--- packages/server/src/shared/typeorm/typeorm.service.ts
+++ packages/server/src/shared/typeorm/typeorm.service.ts
@@ -4,24 +4,26 @@ import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-    @Inject(ConfigService)
-    private readonly config: ConfigService;
+  @Inject(ConfigService)
+  private readonly config: ConfigService;
 
-    public createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: this.config.get<string>('DATABASE_HOST'),
-            port: this.config.get<number>('DATABASE_PORT'),
-            database: this.config.get<string>('DATABASE_NAME'),
-            username: this.config.get<string>('DATABASE_USER'),
-            password: this.config.get<string>('DATABASE_PASSWORD'),
-            entities: ['dist/**/*.entity.{ts,js}'],
-            migrations: ['dist/migrations/*.{ts,js}'],
-            migrationsTableName: 'typeorm_migrations',
-            logger: 'file',
-            logging: true,
-            synchronize: true, // never use TRUE in production!
-
-        };
-    }
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    console.log(this.config.get<string>('DATABASE_HOST'));
+    console.log(this.config.get<string>('DATABASE_PORT'));
+    console.log(this.config.get<string>('DATABASE_NAME'));
+    return {
+      type: 'postgres',
+      host: this.config.get<string>('DATABASE_HOST'),
+      port: this.config.get<number>('DATABASE_PORT'),
+      database: this.config.get<string>('DATABASE_NAME'),
+      username: this.config.get<string>('DATABASE_USER'),
+      password: this.config.get<string>('DATABASE_PASSWORD'),
+      entities: ['dist/**/*.entity.{ts,js}'],
+      migrations: ['dist/migrations/*.{ts,js}'],
+      migrationsTableName: 'typeorm_migrations',
+      logger: 'file',
+      logging: true,
+      synchronize: true, // never use TRUE in production!
+    };
+  }
 }
